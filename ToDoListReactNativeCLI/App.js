7c6737cd17d83d888d64e71ea2d1d135adb0c07f
Dlozi Mthethwa <dlozi.mthethwa@gmail.com>
2023-09-01 13:49:44 +0200

    app

--- ToDoListReactNativeCLI/App.js
+++ ToDoListReactNativeCLI/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function App() {
   return (
     <View>
-      <Text>Dlozi and Ristar</Text>
+      <Text>Dlozi and Ristar in a team</Text>
     </View>
   );
 }
