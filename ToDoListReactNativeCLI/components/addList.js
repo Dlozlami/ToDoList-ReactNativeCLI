627c9d90b7ba894da689aaf1d79f203027e1c8a6
Dlozi Mthethwa <dlozi.mthethwa@gmail.com>
2023-09-01 14:21:00 +0200

    local p

--- ToDoListReactNativeCLI/components/addList.js
+++ ToDoListReactNativeCLI/components/addList.js
@@ -1,10 +1,10 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
-export default function App() {
+export default function AddList() {
   return (
     <View>
-      <Text>Dlozi and Ristar in a team safds thlogi</Text>
+      <Text>addList</Text>
     </View>
   );
 }
